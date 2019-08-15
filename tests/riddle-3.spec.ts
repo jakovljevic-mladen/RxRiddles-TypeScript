db8dcec47c3cb965e2ff5d60784a6c885e8fe0fd
Mladen Jakovljević <jakovljevic.mladen@gmail.com>
2019-08-15 23:39:11 +0200

    Improving Riddle 3 unit test.

--- tests/riddle-3.spec.ts
+++ tests/riddle-3.spec.ts
@@ -6,12 +6,11 @@ describe('Riddle 3', () => {
 
   it('should filter out odd values from [0, 10) array', () => {
     const sourceMarbles = '--a-b-c-d-e-f-g-h-i-j-|';
-    const sourceValues = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8, j: 9 };
-    const expectedMarbles = '--a---b---c---d---e---|';
-    const expectedValues = { a: 0, b: 2, c: 4, d: 6, e: 8 };
+    const expectedMarbles = '--a---c---e---g---i---|';
+    const values = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7, i: 8, j: 9 };
 
-    const source$ = cold(sourceMarbles, sourceValues);
-    const expected$ = cold(expectedMarbles, expectedValues);
+    const source$ = cold(sourceMarbles, values);
+    const expected$ = cold(expectedMarbles, values);
 
     const riddle3 = new Riddle3Solution();
     const result$ = riddle3.solve(source$);
